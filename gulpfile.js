ab7a290f317288f3f78ef03ce63a3565c16f191a
BoBaHbl4 <BoBaHbl4@gmail.com>
2016-02-11 22:01:55 +0200

    Updates for Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -44,3 +44,75 @@ gulp.task('default', ['browser-sync'], function() {
     gulp.watch('./src_dev/html_tmpls/*.*',client);
 
 });
+
+
+
+
+
+
+
+
+
+/*
+ This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+ */
+
+//var gulp = require('gulp');
+//var less = require("gulp-less");
+//var minifyCSS = require('gulp-minify-css');
+//var rename = require('gulp-rename');
+//var inject = require('gulp-inject');
+//var concat = require('gulp-concat');
+//var print = require('gulp-print');
+//var angularFilesort = require('gulp-angular-filesort');
+//var uglify = require('gulp-uglify');
+//var path = require('path');
+//var util = require('gulp-util');
+//var merge = require('merge-stream');
+//
+//// Less Task
+//// Compile *.less-files to css
+//// Minify styles
+//gulp.task('lib-less', function () {
+//    return gulp.src(['./dev_root/css/less/bootstrap/bootstrap.less', './dev_root/css/less/font-awesome/font-awesome.less'])
+//        .pipe(less())
+//        .pipe(minifyCSS())
+//        //.pipe(rename({
+//        //    suffix: ".min"
+//        //}))
+//        .pipe(concat('lib-styles.min.css'))
+//        .pipe(gulp.dest('./dev_root/css'));
+//});
+//
+//gulp.task('less-task', function () {
+//    return gulp.src('./dev_root/css/*.less')
+//        .pipe(less())
+//        .pipe(less().on('error', util.log))
+//        .pipe(concat('main.min.css'))
+//        .pipe(minifyCSS())
+//        .pipe(gulp.dest('./dev_root/css'));
+//});
+//
+//gulp.task('css-inject', function () {
+//    var target = gulp.src('./wwwroot/index.html');
+//    var customCssStream = gulp.src(['./dev_root/css/lib-styles.min.css',
+//        './dev_root/css/main.min.css']);
+//
+//    return target
+//        .pipe(inject(
+//            customCssStream.pipe(print())
+//                .pipe(concat('common.css'))
+//                .pipe(gulp.dest('./wwwroot/css')), { read: false, addRootSlash: false, relative: false })
+//        )
+//        .pipe(gulp.dest('./wwwroot/'));
+//
+//});
+//
+//
+//
+//gulp.task('default', ['lib-less'], function () {
+//    console.log('Hello Gulp!');
+//    gulp.watch('./dev_root/css/less/*.less', ['less-task']);
+//    gulp.watch('./dev_root/css/less/*.less', ['css-inject']);
+//});
